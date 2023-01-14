a1318947e07151062ce50589a8d395e9ab149cd6
Pangwan <80379732+2simo@users.noreply.github.com>
2023-01-14 20:54:52 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['0632114336']
+global.premium = [‘06321143336']
+global.ownernomer = '0632114336'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
